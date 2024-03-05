96c6417f405de378238d23eda2333882e1c9c72f
rafaelsetragni <rafaelsetra@gmail.com>
2024-03-05 09:09:25 -0300

    0.9.1 release

---
+++ Example/Tests/JsonFlattenerTest.swift
@@ -0,0 +1,9 @@
+//
+//  JsonFlattenerTest.swift
+//  IosAwnFcmCore_Tests
+//
+//  Created by Rafael Setragni on 23/12/23.
+//  Copyright © 2023 CocoaPods. All rights reserved.
+//
+
+import Foundation
---
+++ interpreters/JsonFlattener.swift
@@ -0,0 +1,8 @@
+//
+//  JsonFlattener.swift
+//  IosAwnFcmCore
+//
+//  Created by Rafael Setragni on 22/12/23.
+//
+
+import Foundation
